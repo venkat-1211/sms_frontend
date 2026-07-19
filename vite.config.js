5117f67e55c3c42c0c5eedddbfe7141d35b4a37e
venkat-1211 <20venkatesh001@gmail.com>
2026-07-19 13:21:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://smsbackend-production-b03d.up.railway.app',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
